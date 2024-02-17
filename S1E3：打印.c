fa2cc9d653e45245fd470b0f286a6a46d228d803
DNX <109335896+dnx00@users.noreply.github.com>
2024-02-17 22:29:47 +0800

    Create S1E3：打印.c

---
+++ S1E3：打印.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//DNX写于2023年6月20日 
+int main()
+{
+	printf("printf：格式化输出函数\n\
+I love You!\n");//注意：打一个\后可以换行接着写 
+    printf("Hello world\b\b\b\b\bFishC\n");
+    printf("\n\
+         @\n\
+        / \\\n\
+        * *\n\
+        * *\n\
+        * *\n\
+    * * * * * *\n\
+  * * * * * * * *\n\
+* * * * * * * * * *\n\
+        * *\n\
+        * *\n\
+      * * * *\n\
+    * * * * * *\n");
+	return 0;
+ } 
