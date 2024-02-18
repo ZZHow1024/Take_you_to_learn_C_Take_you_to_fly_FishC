bde309dab89c050103239435a960e3acdd524001
DNX <109335896+dnx00@users.noreply.github.com>
2024-02-18 23:55:40 +0800

    Create S1E6：数据类型.c

---
+++ S1E6：数据类型.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//DNX写于2023年6月20日 
+#define j "DNX"
+int main()
+{
+	long long int i;
+	float k;
+	
+	i = 312;
+	k = 1.123456789;
+ 
+	printf("size of int is %d\n",sizeof(int));
+	printf("size of char is %d\n",sizeof(char));
+	printf("size of flout is %d\n",sizeof(float));
+ 	printf("size of %i is %d\n",i,sizeof(i));
+	printf("size of %s is %d\n",j,sizeof(j));
+	printf("size of %f is %d\n",k,sizeof(k));
+ 
+ } 
