7d469eceadf14b5922ee975d3d2623f4eb92a94c
DNX <109335896+dnx00@users.noreply.github.com>
2024-02-28 21:25:59 +0800

    Create S1E11：if语句.c

---
+++ S1E11：if语句.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//DNX写于2023年6月22日 
+int main()
+{
+	//程序说明：90分及以上A，80~90为B，70~80为C，60~70为D，低于60分为E 
+	int i;
+	
+	printf("请输入你的分数：\n");
+	scanf("%d",&i);
+	
+	if (i >= 90)
+	{
+		printf("你的分数评级为A\n");
+	}
+	else if(i >=80 && i < 90) 
+	{
+		printf("你的分数评级为B\n");
+	}
+	else if(i >=70 && i < 80) 
+	{
+		printf("你的分数评级为C\n");
+	}
+	else if(i >=60 && i < 70) 
+	{
+		printf("你的分数评级为D\n");
+	}
+	else if(i>=0 && i < 60) 
+	{
+		printf("你的分数评级为E\n");
+	}
+	else
+	{
+		printf("你这是乱输的数字吧:(\n"); 
+	}
+	return 0;
+ } 
