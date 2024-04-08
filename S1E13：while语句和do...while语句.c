9b3f06fe048f4ab9ba4b586295aa696fb4321995
DNX <109335896+dnx00@users.noreply.github.com>
2024-04-08 23:24:54 +0800

    Create S1E13：while语句和do...while语句.c

---
+++ S1E13：while语句和do...while语句.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+//DNX写于 2023年6月22日 
+
+int main()
+{
+	//1、用while语句实现计算 1+2+3+...+100 的结果
+	printf("程序1：用while语句实现计算 1+2+3+...+100 的结果\n");
+	
+	int i = 1,sum = 0;
+	
+	while (i <= 100)
+	{
+		sum = sum + i;
+		i = i + 1;
+	}
+	printf("结果是%d\n\n",sum);
+	
+	
+	//2、用do...while语句实现计算 1+2+3+...+100 的结果
+	printf("程序2：用do...while语句实现计算 1+2+3+...+100 的结果\n");
+	
+	int ii = 1,sum1 = 0;
+	
+	do
+	{
+		sum1 = sum1 + ii;
+		ii = ii + 1;
+	}
+	while(ii <= 100);
+	printf("结果是%i\n\n",sum1);
+	
+	
+	//3、统计从键盘输入的一行英文句子的字符个数 
+	printf("程序3：统计从键盘输入的一行英文句子的字符个数\n");
+	
+	int count = 0;
+	
+	printf("请开始输入字符(结束请按Enter)："); 
+	while(getchar() != '\n')
+	{
+		count = count + 1;
+	} 
+	
+	printf("你总共输入了%d个字符！\n",count);
+	
+	
+	return 0;
+}
